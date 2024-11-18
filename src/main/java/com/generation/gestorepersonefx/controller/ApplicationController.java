a3ff4badde807263863b5b42090d1831f0ef38e5
StefanoR <rubinetti.stefano@gmail.com>
2024-11-18 14:39:15 +0100

    repo aggiunta

--- src/main/java/com/generation/gestorepersonefx/controller/ApplicationController.java
+++ src/main/java/com/generation/gestorepersonefx/controller/ApplicationController.java
@@ -1,5 +1,25 @@
 package com.generation.gestorepersonefx.controller;
 
+import com.generation.gestorepersonefx.helpers.ControllerHelper;
+import com.generation.gestorepersonefx.model.Person;
+import com.generation.gestorepersonefx.model.PersonRepository;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.sql.SQLException;
+
 public class ApplicationController
 {
+	private ControllerHelper helper = ControllerHelper.getInstance();
+
+
+	@FXML
+	private Text peopleList;
+
+	public void riempiCasellaTesto()
+	{
+		String contenuto = helper.produciListaDaStampareASchermo();
+
+		peopleList.setText(contenuto);
+	}
 }
---
+++ src/main/java/com/generation/gestorepersonefx/helpers/ControllerHelper.java
@@ -0,0 +1,47 @@
+package com.generation.gestorepersonefx.helpers;
+
+import com.generation.gestorepersonefx.model.Person;
+import com.generation.gestorepersonefx.model.PersonRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ControllerHelper
+{
+	private static ControllerHelper instance;
+	public static ControllerHelper getInstance()
+	{
+		if(instance == null)
+			instance = new ControllerHelper();
+
+		return instance;
+	}
+	private ControllerHelper(){}
+
+	private PersonRepository repo = PersonRepository.getInstance();
+
+	public String produciListaDaStampareASchermo()
+	{
+		try
+		{
+			List<Person> all = repo.findAllPeople();
+			StringBuilder res = new StringBuilder();
+
+			for(Person p : all)
+			{
+				res.append(p.getId()+" ");
+				res.append(p.getName()+" ");
+				res.append(p.getSurname()+"\n");
+			}
+
+			return res.toString();
+		}catch (SQLException e)
+		{
+			return "Problema con il db";
+		}
+	}
+
+
+
+
+}
